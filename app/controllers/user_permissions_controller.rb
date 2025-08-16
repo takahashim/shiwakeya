2e0606b8e901d68c41c3d7f42a7e873983663281
takahashim <takahashimm@gmail.com>
2025-08-17 02:27:36 +0900

    bin/brakeman --no-pager

--- app/controllers/user_permissions_controller.rb
+++ app/controllers/user_permissions_controller.rb
@@ -13,8 +13,7 @@ class UserPermissionsController < ApplicationController
   end
 
   def update
-    if @user.update(user_params)
-      update_spreadsheet_permissions
+    if update_user_role && update_spreadsheet_permissions
       redirect_to user_permissions_path, notice: "ユーザー権限を更新しました"
     else
       @spreadsheets = Spreadsheet.all
@@ -29,12 +28,21 @@ class UserPermissionsController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def user_params
-    params.require(:user).permit(:role)
+  def update_user_role
+    # Safely update role without mass assignment vulnerability
+    role = params.dig(:user, :role)
+
+    # Only update if role is valid
+    if role.present? && User.roles.keys.include?(role)
+      @user.role = role
+      @user.save
+    else
+      true # Return true if no role update requested
+    end
   end
 
   def update_spreadsheet_permissions
-    return unless @user.role_member?
+    return true unless @user.role_member?
 
     existing_permissions = @user.user_spreadsheet_permissions.index_by(&:spreadsheet_id)
 
@@ -51,5 +59,7 @@ class UserPermissionsController < ApplicationController
     params[:remove_permissions]&.each do |spreadsheet_id|
       @user.user_spreadsheet_permissions.where(spreadsheet_id: spreadsheet_id).destroy_all
     end
+
+    true
   end
 end
